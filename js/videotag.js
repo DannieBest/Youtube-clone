271c2992e216c6e6bf7ba79a4d6b921194264f1c
HOLYdave_studios <HOLYdave.com>
2026-07-10 21:11:23 +0100

    added javascript to target my right arrow

--- js/videotag.js
+++ js/videotag.js
@@ -1,12 +1,19 @@
-const container = document.querySelector(".video-tag__container");
+const container = document.querySelector('.video-tag__container');
 
-const leftArrow = document.querySelector(".video-tag__arrow--left");
+const leftArrow = document.querySelector('.video-tag__arrow--left');
 
-const rightArrow = document.querySelector(".video-tag__arrow--right");
+const rightArrow = document.querySelector('.video-tag__arrow--right');
 
-leftArrow.addEventListener("click", () => {
+leftArrow.addEventListener('click', () => {
   container.scrollBy({
     left: -300,
-    behavior: "smooth"
+    behavior: 'smooth',
+  });
+});
+
+rightArrow.addEventListener('click', () => {
+  container.scrollBy({
+    left: 300,
+    behavior: 'smooth',
   });
 });
