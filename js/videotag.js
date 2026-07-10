a1bedc058deadd573ed2824f40e074c092215bff
HOLYdave_studios <HOLYdave.com>
2026-07-10 22:41:01 +0100

    added javascript to hide scrool button when at the extreme end

--- js/videotag.js
+++ js/videotag.js
@@ -4,6 +4,10 @@ const leftArrow = document.querySelector('.video-tag__arrow--left');
 
 const rightArrow = document.querySelector('.video-tag__arrow--right');
 
+window.addEventListener("resize", updateArrow);
+
+container.addEventListener("scroll", updateArrow);
+
 leftArrow.addEventListener('click', () => {
   container.scrollBy({
     left: -300,
@@ -17,3 +21,20 @@ rightArrow.addEventListener('click', () => {
     behavior: 'smooth',
   });
 });
+
+function updateArrow () {
+  if (container.scrollLeft <= 0) {
+  leftArrow.style.display = 'none';
+  } else {
+  leftArrow.style.display = 'flex';
+  }
+
+  if (container.scrollLeft >= container.scrollWidth - container.clientWidth) {
+    rightArrow.style.display = 'none';
+  } else {
+    rightArrow.style.display = 'flex';
+  }
+};
+
+updateArrow();
+
