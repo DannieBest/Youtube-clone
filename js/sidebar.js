a19c79d34658cfc14316968553c752a652eb520c
HOLYdave_studios <HOLYdave.com>
2026-07-10 19:52:22 +0100

    finished adding javascript for sidebar to colapse at desktop size

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,16 @@
 const sidebarToggle = document.querySelector('.sidebar__toggle');
 const sidebar = document.querySelector('aside');
 
-sidebarToggle.addEventListener('click', () => {
-  sidebar.classList.toggle('collapsed');
+if (sidebarToggle && sidebar) {
+  sidebarToggle.addEventListener('click', () => {
+    sidebar.classList.toggle('collapsed');
+  });
+}
+
+
+const toggleButton = document.querySelector(".sidebar__toggle");
+const sidebar = document.querySelector("aside");
+
+toggleButton.addEventListener("click", () => {
+    sidebar.classList.toggle("collapsed");
 });
